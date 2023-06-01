a1c3b5193d228ddd64f5bbb6820317be300a170d
abhishek <abhishek.nt94@gmail.com>
2023-06-01 12:37:28 -0700

    Added form

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  // const [newtitle, setNewTitle] = useState("")
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = ()=>{
+  //   setTitle(newtitle);
+  // }
+
+  // const changeHandler = (event)=>{
+  //   setNewTitle(event.target.value);
+  // }
+
+
+ 
   
   return (
     <div className="expense-item">
@@ -12,6 +25,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* <input type="text" value={newtitle} onChange={changeHandler}/>
+      <button onClick={clickHandler}>Change Title</button> */}
     </div>
   );
 };
---
+++ src/components/NewExpenses.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NewExpenses.css'
+const NewExpenses = () => {
+  return (
+    <div className='new-expense'>
+        <form action="">
+            
+        </form>
+      
+    </div>
+  )
+}
+
+export default NewExpenses
